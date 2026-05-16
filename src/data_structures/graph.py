75c216b4e7f6ad2ae143c868b63d1b659a9f0196
oranganip <hanif0947ft.2025@student.uny.ac.id>
2026-05-16 20:54:01 +0700

    Implement Binary Search Tree (BST) class

---
+++ src/data_structures/graph.py
@@ -0,0 +1,35 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if not self.root:
+            self.root = TreeNode(data)
+        else:
+            self._insert_rec(self.root, data)
+
+    def _insert_rec(self, node, data):
+        if data < node.data:
+            if not node.left:
+                node.left = TreeNode(data)
+            else:
+                self._insert_rec(node.left, data)
+        else:
+            if not node.right:
+                node.right = TreeNode(data)
+            else:
+                self._insert_rec(node.right, data)
+
+    def inorder(self, node):
+        if node:
+            self.inorder(node.left)
+            print(node.data, end=" ")
+            self.inorder(node.right)
+
+
