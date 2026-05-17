d28fd3da63ace98e524b98bfd61fdf9e082c91ed
oranganip <hanif0947ft.2025@student.uny.ac.id>
2026-05-17 12:45:53 +0700

    ngetest LL

--- src/data_structures/linked_list.py
+++ src/data_structures/linked_list.py
@@ -1,26 +1,44 @@
+from typing import Optional, Any
+
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+    def __init__(self, data: Any):
+        self.data: Any = data
+        self.next: Optional['Node'] = None
 
 class LinkedList:
     def __init__(self):
-        self.head = None
+        self.head: Optional[Node] = None
 
-    def append(self, data):
+    def append(self, data: Any) -> None:
         new_node = Node(data)
-        if not self.head:
+        if self.head is None:
             self.head = new_node
             return
-        last = self.head
-        while last.next:
-            last = last.next
-        last.next = new_node
-
-    def display(self):
         current = self.head
-        nodes = []
-        while current:
-            nodes.append(str(current.data))
+        while current.next is not None:
             current = current.next
-        print(" -> ".join(nodes) + " -> None")
+        current.next = new_node
+
+    def delete(self, key: Any) -> bool:
+        if self.head is None:
+            return False
+
+        if self.head.data == key:
+            self.head = self.head.next
+            return True
+
+        current = self.head
+        while current.next is not None:
+            if current.next.data == key:
+                current.next = current.next.next
+                return True
+            current = current.next
+
+        return False
+
+    def display(self) -> None:
+        current = self.head
+        while current is not None:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
